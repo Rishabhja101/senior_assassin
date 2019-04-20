c6c497def6ec5acd81645b0f36b914139f4674f4
Kyle Mumma <kyle.mumma@live.com>
2019-04-20 00:57:48 -0700

    connected to aws backend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Request from 'superagent';
 import GoogleLogin from 'react-google-login';
 
 class App extends React.Component {
@@ -6,10 +7,12 @@ class App extends React.Component {
     super(props);
     this.state = {
       loggedIn: false,
-      error: false,
+      error: '',
       name: '',
       email: '',
       target: 'blank',
+      participants: [],
+      data: null
     }
 
     this.onSignIn = this.onSignIn.bind(this);
@@ -17,23 +20,40 @@ class App extends React.Component {
 
   componentDidMount() {
     //make those MF API calls BOIIIIII
+    Request.get("http://seniorassassin.us-west-2.elasticbeanstalk.com/participants")
+    .then((res) => {
+      let participantsArr = [];
+      for(let participant of res.body){
+        participantsArr.push(participant["name"]);
+      }
+      this.setState({
+        data: res.body,
+        participants: participantsArr
+      });
+    })
   }
 
   onSignIn(googleUser) {
     const profile = googleUser.getBasicProfile();
-    const name = profile.getName();
+    const name = profile.getName().toLowerCase();
     const email = profile.getEmail();
     const emailSubdomain = email.substring(email.indexOf("@"));
 
+    console.log(name);
+    console.log(this.state.participants);
     // not signed in with student email
     if(emailSubdomain !== "@apps.nsd.org"){
       this.setState({
-        error: true
+        error: 'non_nsd'
       })
+    } else if(!this.state.participants.includes(name)){ //not a participant
+      this.setState({
+        error: 'non_participant'
+      });
     } else {
       this.setState({
         loggedIn: true,
-        error: false,
+        error: '',
         name: name,
         email: email
       });
@@ -41,8 +61,8 @@ class App extends React.Component {
   }
 
   render() {
-    //if logged in
-    if(this.state.loggedIn && !this.state.error){
+    //if logged in and no errors
+    if(this.state.loggedIn && this.state.error === ''){
       return(
         <div className="App text-center container">
           <h1 className="display-4">NCHS Senior Assassin</h1>
@@ -54,8 +74,14 @@ class App extends React.Component {
 
     //if not logged in or error
     let errorMessage = null;
-    if(this.state.error){
+    if(this.state.error === 'non_nsd'){
       errorMessage = <p className="alert alert-danger">Error: You must log in with your apps.nsd.org student email</p>
+    } else if(this.state.error === 'non_participant'){
+      errorMessage = <p className="alert alert-danger">
+                        Error: You are not registered for Senior Assassin,
+                        if this is an error please contact Monica Potts on instagram
+                        <a href="https://www.instagram.com/monicapottts" target="_blank"> @monicapottts</a>
+                      </p>
     }
 
     return (
