65625c5f1188e7baa6725603bc670bf89af085a5
Kyle Mumma <kyle.mumma@live.com>
2019-05-09 18:56:07 -0700

    added master login for hussain

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends React.Component {
       email: '',
       target: '',
       participants: [],
-      data: null
+      data: null,
+      master: false,
+      masterList: null
     }
 
     this.onSignIn = this.onSignIn.bind(this);
@@ -38,9 +40,40 @@ class App extends React.Component {
     const name = profile.getName().toLowerCase();
     const email = profile.getEmail();
     const emailSubdomain = email.substring(email.indexOf("@"));
-    
-    // not signed in with student email (with added javi check)
-    if(emailSubdomain !== "@apps.nsd.org"){
+
+
+    //hussain master login
+    if(name == 'hussain aladwan'){
+      //api get request for persons target
+      const url = "https://nchsassassin.com/participants";
+      const Cryptr = require('cryptr');
+      const cryptr = new Cryptr('4t7w!z%C&F)J@NcRfUjXn2r5u8x/A?D(');
+
+      Request.get(url)
+      .then((res) => {
+        let response = res.body;
+
+
+        let masterArr = [];
+        for(let participant of response){
+          let target = participant["target"]
+          try {
+            target = cryptr.decrypt(target);
+          } catch(e) {
+            target = "blank";
+          }
+          masterArr.push([participant['name'], target]);
+        }
+
+        this.setState({
+          loggedIn: false,
+          error: '',
+          name: name,
+          master: true,
+          masterList: masterArr
+        });
+      });
+    } else if(emailSubdomain !== "@apps.nsd.org"){ // not signed in with student email
       this.setState({
         error: 'non_nsd'
       })
@@ -68,7 +101,8 @@ class App extends React.Component {
           error: '',
           name: name,
           target: target,
-          email: email
+          email: email,
+          master: false
         });
       });
     }
@@ -85,6 +119,23 @@ class App extends React.Component {
         
           <p style={{"position":"fixed", "bottom":"10px", "left": "50%", "transform": "translateX(-50%)", "width": "100%"}}>Website made by <a href="https://kylemumma.me">Kyle Mumma</a> and 
           <a href="https://linkedin.com/in/rishabhjain101"> Rishabh Jain</a></p>
+        </div>
+      );
+    }
+
+    //if master login
+    if(this.state.master){
+      const targetsObject = this.state.masterList.map(x => <li className="list-group-item">{x[0]} : {x[1]}</li>);
+
+      return(
+        <div className="App text-center container">
+          <h1 className="display-4">NCHS Senior Assassin</h1>
+          <p className="lead">Welcome back daddy {this.state.name}</p>
+
+          <ul className="list-group">
+            <li className="list-group-item active">Targets</li>
+            {targetsObject}
+          </ul>
         </div>
       );
     }
